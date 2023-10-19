260e543c288cd281e3ca9ae63d5144202101840d
andersonvilla <estiven.villa@udea.edu.co>
2023-10-18 19:44:59 -0500

    Header component

---
+++ app/src/main/java/com/co/edu/udea/compumovil/gr06_2023_2/tripwithus/components/Header.kt
@@ -0,0 +1,49 @@
+package com.co.edu.udea.compumovil.gr06_2023_2.tripwithus.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun HeaderComponent(text: String) {
+    val headerModifier = Modifier
+        .fillMaxSize()
+        .background(MaterialTheme.colorScheme.onPrimary)
+
+    val contentModifier = Modifier
+        .padding(16.dp)
+
+
+    Box(
+        modifier = headerModifier
+    ) {
+        Row(
+            modifier = Modifier
+                .background(Color(0xFF98C1D9))
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+
+        ) {
+
+            Text(
+                text = text,
+                modifier = contentModifier,
+                fontSize = 20.sp,
+                color = Color(0xFF3D5A80)
+            )
+        }
+
+    }
+}
+
